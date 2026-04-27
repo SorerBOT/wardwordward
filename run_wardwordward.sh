4846f167fd3abdfd39db0a7731e05bf5ca917301
SorerBOT <alonfiller@gmail.com>
2026-04-27 21:59:18 +0300

    FEAT corrected fzf flow

--- run_wardwordward.sh
+++ run_wardwordward.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gcc wardwordward.c -o wardwordward
-./wardwordward
+echo "" | fzf --disabled --ansi --query="" --bind "change:reload(./wardwordward {q})"
--- wardwordward.c
+++ wardwordward.c
@@ -1,3 +1,10 @@
-int main()
+#include <stdio.h>
+
+int main(int argc, char** argv)
 {
+    printf("bonjour: hello\n");
+    printf("parler: to talk\n");
+    printf("discuter: to discuss\n");
+
+    return 0;
 }
